11c740573885d4b5250aaf71880568dff076c84c
Karolina Rowińska (Uczeń) <karrow03@gmail.com>
2022-03-22 10:11:29 +0100

    algorytm liczb pierwszych

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,30 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    int lp = 0;
+    int p = 2;
+    int n;
+    int d;
+    bool gg;
+    cout << "podaj ile liczb" << endl;
+    cin >> n;
+    
+    while (lp < n) {
+        gg = true;
+        for (int d = 2; d <= p - 1; d++) {
+            if (p % d == 0) {
+                gg = false;
+            }
+        }
+        if (gg==true) {
+            cout << p << "jest pierwsze" << endl;
+            lp = lp + 1;
+        }
+        p = p + 1;
+    } 
 }
