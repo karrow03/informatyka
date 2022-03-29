6927e2349bfc9e4c0c846ef218fe4cd61730aa54
Karolina Rowińska (Uczeń) <karrow03@gmail.com>
2022-03-29 09:54:27 +0200

    nww i nwd

---
+++ fbbtnbntbtbebbbtgbebrbb/fbbtnbntbtbebbbtgbebrbb.cpp
@@ -0,0 +1,34 @@
+﻿// fbbtnbntbtbebbbtgbebrbb.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int iloczyn;
+    int nww;
+    cout << "podaj liczbe a " << endl;
+    cin >> a;
+    cout << "podaj liczbe b " << endl;
+    cin >> b;
+
+    iloczyn = a * b;
+
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+    }
+    cout << "Najwiekszy wspolny dzielnik to " << a << endl;
+
+    nww = iloczyn / a;
+
+    cout << "Najmniejsza wspolna wielokrotnosc to " << nww << endl;
+ 
+}
